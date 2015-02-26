4dd280e43f2063aa4f77209f9e064d04a9ef283d
Zenildo Pinto <zenildopinto@hotmail.com>
2015-02-26 00:11:28 +0000

    changing my Main.php

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Teste GIT");
+		System.out.println("E agora o que temos ");
+		System.out.println("E agora o que temos k ");
     }
 }
